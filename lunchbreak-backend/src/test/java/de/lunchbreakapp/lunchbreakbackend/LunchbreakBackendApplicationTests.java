cab923e2379d23c5e344fe3fbffd08f308db8780
Franziska Georg <franzi.georg@web.de>
2020-07-07 13:05:34 +0200

    Initial commit

---
+++ lunchbreak-backend/src/test/java/de/lunchbreakapp/lunchbreakbackend/LunchbreakBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lunchbreakapp.lunchbreakbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchbreakBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
