9c3b17b2041a3a88ec1e545dec0da3a1eb413502
Franziska Georg <franzi.georg@web.de>
2020-07-07 17:54:46 +0200

    Add MatchCard component and random user fetch function

--- lunchbreak-frontend/src/App.js
+++ lunchbreak-frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import MatchCard from "./components/MatchCard";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <MatchCard/>
     </div>
   );
 }
-
-export default App;
