b34cd37e5b79d700cd2f3c44e777d6ce025b76f3
Franziska Georg <franzi.georg@web.de>
2020-07-10 21:16:25 +0200

    Build basic login page and functionality in frontend

--- lunchbreak-frontend/src/App.js
+++ lunchbreak-frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import MatchCard from "./components/MatchCard";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import UserContextProvider from "./context/user/UserContextProvider";
+
+function Navigation() {
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/login" component={LoginPage}/>
+            <Route path="/" component={MatchCard} exact/>
+        </Switch>
+    </BrowserRouter>
+}
 
 export default function App() {
-  return (
-    <div className="App">
-      <MatchCard/>
-    </div>
-  );
+    return (
+        <UserContextProvider>
+            <Navigation/>
+        </UserContextProvider>
+    )
 }
