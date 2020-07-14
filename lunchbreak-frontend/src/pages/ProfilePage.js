beb209b4ec2cc13d9a2f46a9f1f48a3516eda774
Franziska Georg <franzi.georg@web.de>
2020-07-14 14:51:18 +0200

    Add further input fields and checkbox set to profile page

--- lunchbreak-frontend/src/pages/ProfilePage.js
+++ lunchbreak-frontend/src/pages/ProfilePage.js
@@ -6,6 +6,11 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 const useStyles = makeStyles((theme) => ({
     gridContainer: {
@@ -26,6 +31,9 @@ const useStyles = makeStyles((theme) => ({
     },
     notchedOutline: {
         borderColor: "#eef5f6 !important",
+    },
+    nextTopic: {
+        paddingTop: theme.spacing(2)
     }
 }));
 
@@ -60,6 +68,15 @@ export default function ProfilePage() {
         }
     ]
 
+    const [lunchdays, setLunchdays] = useState({
+        monday: false,
+        tuesday: false,
+        wednesday: false,
+        thursday: false,
+        friday: false
+    })
+    const {monday, tuesday, wednesday, thursday, friday} = lunchdays;
+
     const [profileInput, setProfileInput] = useReducer(
         (state, newState) => ({...state, ...newState}),
         {
@@ -83,6 +100,10 @@ export default function ProfilePage() {
 
     function handleSubsidiaryChange(event) {
         setSubsidiary(event.target.value);
+    }
+
+    function handleLunchdayChange(event) {
+        setLunchdays({...lunchdays, [event.target.name]: event.target.checked});
     }
 
     return (
@@ -178,6 +199,127 @@ export default function ProfilePage() {
                                 </MenuItem>
                             ))}
                         </TextField>
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            required
+                            margin="dense"
+                            className={classes.inputField}
+                            InputProps={{
+                                classes: {
+                                    notchedOutline: classes.notchedOutline
+                                }
+                            }}
+                            id="outlined-required"
+                            variant="outlined"
+                            name="favoriteFood"
+                            label="Lieblingsessen bzw. -essensrichtung"
+                            value={profileInput.favoriteFood}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            required
+                            multiline
+                            margin="dense"
+                            className={classes.inputField}
+                            InputProps={{
+                                classes: {
+                                    notchedOutline: classes.notchedOutline
+                                }
+                            }}
+                            id="outlined-required"
+                            variant="outlined"
+                            name="interests"
+                            label="Hobbies oder Interessen"
+                            value={profileInput.interests}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item
+                          className={classes.nextTopic}
+                    >
+                        <Typography variant="h5">
+                            Kontaktdaten
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            required
+                            margin="dense"
+                            className={classes.inputField}
+                            InputProps={{
+                                classes: {
+                                    notchedOutline: classes.notchedOutline
+                                }
+                            }}
+                            id="outlined-required"
+                            variant="outlined"
+                            name="email"
+                            label="E-Mail-Adresse"
+                            value={profileInput.email}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            required
+                            margin="dense"
+                            className={classes.inputField}
+                            InputProps={{
+                                classes: {
+                                    notchedOutline: classes.notchedOutline
+                                }
+                            }}
+                            id="outlined-required"
+                            variant="outlined"
+                            name="phoneNumber"
+                            label="Handynummer"
+                            value={profileInput.phoneNumber}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item
+                          className={classes.nextTopic}
+                    >
+                        <Typography variant="h5">
+                            Deine Lunchdays
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <FormControl component="fieldset">
+                            <FormLabel
+                                component="legend"
+                                className={classes.nextTopic}>
+                                Markiere die Tage, an denen Du grundsätzlich lunchen gehen möchtest/kannst
+                            </FormLabel>
+                            <FormGroup>
+                                <FormControlLabel
+                                    control={<Checkbox checked={monday} onChange={handleLunchdayChange} name="monday"/>}
+                                    label="Montag"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={tuesday} onChange={handleLunchdayChange}
+                                                       name="tuesday"/>}
+                                    label="Dienstag"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={wednesday} onChange={handleLunchdayChange}
+                                                       name="wednesday"/>}
+                                    label="Mittwoch"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={thursday} onChange={handleLunchdayChange}
+                                                       name="thursday"/>}
+                                    label="Donnerstag"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox checked={friday} onChange={handleLunchdayChange} name="friday"/>}
+                                    label="Freitag"
+                                />
+                            </FormGroup>
+                        </FormControl>
                     </Grid>
                 </CardContent>
             </Grid>
