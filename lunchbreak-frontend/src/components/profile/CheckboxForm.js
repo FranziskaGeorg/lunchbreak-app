2b7b203ea3c4b1c11ed2b66cf1474ebff241bdcf
Franziska Georg <franzi.georg@web.de>
2020-07-14 16:29:32 +0200

    Tidy up profile components

---
+++ lunchbreak-frontend/src/components/profile/CheckboxForm.js
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+
+const useStyles = makeStyles((theme) => ({
+    nextTopic: {
+        paddingTop: theme.spacing(2)
+    }
+}));
+
+export default function CheckboxForm() {
+    const classes = useStyles();
+
+    const [lunchdays, setLunchdays] = useState({
+        monday: false,
+        tuesday: false,
+        wednesday: false,
+        thursday: false,
+        friday: false
+    })
+
+    const {monday, tuesday, wednesday, thursday, friday} = lunchdays;
+
+    function handleLunchdayChange(event) {
+        setLunchdays({...lunchdays, [event.target.name]: event.target.checked});
+    }
+
+    function LunchdayCheckbox({checked, fieldName, label}) {
+        return (
+        <FormControlLabel
+            control={<Checkbox checked={checked}
+                               onChange={handleLunchdayChange}
+                               name={fieldName}/>}
+            label={label}
+        />
+        )
+    }
+
+    return (
+        <FormControl component="fieldset">
+            <FormLabel
+                component="legend"
+                className={classes.nextTopic}>
+                Markiere die Tage, an denen Du grundsätzlich lunchen gehen möchtest/kannst
+            </FormLabel>
+            <FormGroup>
+                <LunchdayCheckbox checked={monday} fieldName="monday" label="Montag"/>
+                <LunchdayCheckbox checked={tuesday} fieldName="tuesday" label="Dienstag"/>
+                <LunchdayCheckbox checked={wednesday} fieldName="wednesday" label="Mittwoch"/>
+                <LunchdayCheckbox checked={thursday} fieldName="thursday" label="Donnerstag"/>
+                <LunchdayCheckbox checked={friday} fieldName="tuesday" label="Freitag"/>
+            </FormGroup>
+        </FormControl>
+    )
+}
--- lunchbreak-frontend/src/pages/ProfilePage.js
+++ lunchbreak-frontend/src/pages/ProfilePage.js
@@ -1,94 +1,19 @@
-import React, {useReducer, useState} from "react";
+import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import MenuItem from "@material-ui/core/MenuItem";
-import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import LunchdayCheckboxes from "../components/profile/LunchdayCheckboxes";
-import ContactDetailsForm from "../components/profile/ContactDetailsForm";
+import ProfileForm from "../components/profile/ProfileForm";
 
 const useStyles = makeStyles((theme) => ({
     gridContainer: {
         paddingTop: theme.spacing(4),
         paddingBottom: theme.spacing(4)
-    },
-    inputField: {
-        backgroundColor: "#eef5f6",
-        '@media (max-width: 599px)': {
-            width: "75vw"
-        },
-        '@media (min-width:600px)': {
-            width: "50vw"
-        },
-        '@media (min-width:960px)': {
-            width: "25vw"
-        }
-    },
-    notchedOutline: {
-        borderColor: "#eef5f6 !important",
-    },
-    nextTopic: {
-        paddingTop: theme.spacing(2)
     }
 }));
 
 export default function ProfilePage() {
     const classes = useStyles();
-    const [subsidiary, setSubsidiary] = useState('');
-
-    const subsidiaries = [
-        {
-            value: "Technologies"
-        },
-        {
-            value: "Business Consultants"
-        },
-        {
-            value: "Communications"
-        },
-        {
-            value: "Development & Integration"
-        },
-        {
-            value: "Services"
-        },
-        {
-            value: "Solutions"
-        },
-        {
-            value: "Babiel"
-        },
-        {
-            value: "PROCON IT"
-        }
-    ]
-
-    const [profileInput, setProfileInput] = useReducer(
-        (state, newState) => ({...state, ...newState}),
-        {
-            firstName: '',
-            lastName: '',
-            job: '',
-            subsidiary: subsidiary,
-            favoriteFood: '',
-            interests: '',
-            email: '',
-            phoneNumber: '',
-            lunchDays: [] // hier noch die Connection zu den Checkboxen schaffen!,
-        }
-    )
-
-    function handleInputChange(event) {
-        const field = event.target.name;
-        const value = event.target.value;
-        setProfileInput({[field]: value});
-    }
-
-    function handleSubsidiaryChange(event) {
-        setSubsidiary(event.target.value);
-    }
 
     return (
         <Card>
@@ -100,141 +25,7 @@ export default function ProfilePage() {
                   spacing={1}
             >
                 <CardContent>
-                    <Grid item>
-                        <Typography variant="h5">
-                            Über Dich
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            required
-                            margin="dense"
-                            className={classes.inputField}
-                            InputProps={{
-                                classes: {
-                                    notchedOutline: classes.notchedOutline
-                                }
-                            }}
-                            id="outlined-required"
-                            variant="outlined"
-                            name="firstName"
-                            label="Vorname"
-                            value={profileInput.firstName}
-                            onChange={handleInputChange}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            required
-                            margin="dense"
-                            className={classes.inputField}
-                            InputProps={{
-                                classes: {
-                                    notchedOutline: classes.notchedOutline
-                                }
-                            }}
-                            id="outlined-required"
-                            variant="outlined"
-                            name="lastName"
-                            label="Nachname"
-                            value={profileInput.lastName}
-                            onChange={handleInputChange}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            required
-                            margin="dense"
-                            className={classes.inputField}
-                            InputProps={{
-                                classes: {
-                                    notchedOutline: classes.notchedOutline
-                                }
-                            }}
-                            id="outlined-required"
-                            variant="outlined"
-                            name="job"
-                            label="Tätigkeit bei CONET"
-                            value={profileInput.job}
-                            onChange={handleInputChange}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            required
-                            select
-                            margin="dense"
-                            className={classes.inputField}
-                            InputProps={{
-                                classes: {
-                                    notchedOutline: classes.notchedOutline
-                                }
-                            }}
-                            id="outlined-select"
-                            variant="outlined"
-                            name="subsidiary"
-                            label="CONET-Gesellschaft"
-                            value={subsidiary}
-                            onChange={handleSubsidiaryChange}
-                        >
-                            {subsidiaries.map(subsidiary => (
-                                <MenuItem value={subsidiary.value}>
-                                    {subsidiary.value}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            required
-                            margin="dense"
-                            className={classes.inputField}
-                            InputProps={{
-                                classes: {
-                                    notchedOutline: classes.notchedOutline
-                                }
-                            }}
-                            id="outlined-required"
-                            variant="outlined"
-                            name="favoriteFood"
-                            label="Lieblingsessen bzw. -essensrichtung"
-                            value={profileInput.favoriteFood}
-                            onChange={handleInputChange}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            required
-                            multiline
-                            margin="dense"
-                            className={classes.inputField}
-                            InputProps={{
-                                classes: {
-                                    notchedOutline: classes.notchedOutline
-                                }
-                            }}
-                            id="outlined-required"
-                            variant="outlined"
-                            name="interests"
-                            label="Hobbies oder Interessen"
-                            value={profileInput.interests}
-                            onChange={handleInputChange}
-                        />
-                    </Grid>
-                    <Grid item className={classes.nextTopic}>
-                        <Typography variant="h5">
-                            Kontaktdaten
-                        </Typography>
-                    </Grid>
-                    <ContactDetailsForm handleInputChange={handleInputChange} profileInput={profileInput}/>
-                    <Grid item className={classes.nextTopic}>
-                        <Typography variant="h5">
-                            Deine Lunchdays
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <LunchdayCheckboxes/>
-                    </Grid>
+                    <ProfileForm/>
                 </CardContent>
             </Grid>
         </Card>
