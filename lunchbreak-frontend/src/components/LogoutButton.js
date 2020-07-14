ca923ed91ccec4991c17089d8dbf2a9489ac555c
Franziska Georg <franzi.georg@web.de>
2020-07-14 08:54:57 +0200

    Remove unused vars in frontend

--- lunchbreak-frontend/src/components/LogoutButton.js
+++ lunchbreak-frontend/src/components/LogoutButton.js
@@ -1,12 +1,10 @@
 import React, {useContext} from "react";
 import SvgIcon from "@material-ui/core/SvgIcon";
 import {FaSignOutAlt} from "react-icons/all";
-import {useLocation} from "react-router";
 import {LOGOUT} from "../context/user/UserContextProvider";
 import {UserDispatchContext} from "../context/user/UserContext";
 
 export default function LogoutButton() {
-    const location = useLocation();
     const dispatch = useContext(UserDispatchContext);
 
     function logout() {
