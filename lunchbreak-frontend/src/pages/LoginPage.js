9281e1c33020ad9e092b5c1a050ed114173f33fa
Franziska Georg <franzi.georg@web.de>
2020-07-17 11:39:50 +0200

    Move login form to its own component

--- lunchbreak-frontend/src/pages/LoginPage.js
+++ lunchbreak-frontend/src/pages/LoginPage.js
@@ -1,18 +1,8 @@
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
 import React, {useContext} from "react";
-import {Redirect} from "react-router-dom";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import {LOGIN, LOGIN_FAILED, LOGIN_SUCCESS} from "../context/user/UserContextProvider";
-import {UserDispatchContext, UserStateContext} from "../context/user/UserContext";
-import {performLogin} from "../utils/AuthUtils";
-import {getDecodedJWTToken, setJWTToken} from "../utils/JWTUtils";
 import Card from "@material-ui/core/Card";
-import Typography from "@material-ui/core/Typography";
-import ButtonYellowBig from "../components/buttons/ButtonYellowBig";
-import {Form, Formik} from "formik";
-import * as Yup from "yup";
-import InputTextFieldValidated from "../components/inputFields/InputTextFieldValidated";
+import LoginForm from "../components/inputForms/LoginForm";
 
 const useStyles = makeStyles((theme) => ({
     gridBigContainer: {
@@ -36,30 +26,6 @@ const useStyles = makeStyles((theme) => ({
 export default function LoginPage() {
     const classes = useStyles();
 
-    const dispatch = useContext(UserDispatchContext);
-
-    function login(username, password) {
-        dispatch({type: LOGIN});
-        performLogin(username, password)
-            .then((data) => {
-                setJWTToken(data);
-                const userData = getDecodedJWTToken();
-                dispatch({type: LOGIN_SUCCESS, payload: userData});
-            })
-            .catch(() => {
-                dispatch({type: LOGIN_FAILED});
-            });
-    }
-
-    const {authStatus} = useContext(UserStateContext);
-    if (authStatus === 'SUCCESS') {
-        return <Redirect to={'/dailymatch'}/>;
-    }
-
-    function goToRegistration() {
-        window.location.href = "/register";
-    }
-
     return (
         <Grid container
               className={classes.gridBigContainer}
@@ -74,65 +40,7 @@ export default function LoginPage() {
                           alignContent="center"
                           justify="center"
                     >
-                        <Grid item>
-                            <Typography variant="h4" color="primary" align="center">
-                                Welcome to LunchBreak
-                            </Typography>
-
-                            <Formik initialValues={
-                                {
-                                    username: '',
-                                    password: ''
-                                }}
-                                    onSubmit={(values) => {
-                                        login(values.username, values.password);
-                                        console.log(values);
-                                    }}
-
-                                    validationSchema={Yup.object().shape({
-                                        username: Yup.string()
-                                            .required("Pflichtfeld")
-                                            .email("Keine valide E-Mail-Adresse"),
-                                        password: Yup.string()
-                                            .required("Pflichtfeld")
-                                    })}
-                            >
-                                {props => {
-                                    const {
-                                        values,
-                                        touched,
-                                        errors,
-                                        handleChange,
-                                        handleBlur,
-                                        handleSubmit
-                                    } = props;
-                                    return (
-                                        <Form>
-                                            <Grid item className={classes.nextTopic}>
-                                                <InputTextFieldValidated fieldType="text" fieldName="username"
-                                                                         label="E-Mail-Adresse" formikProps={props}/>
-                                            </Grid>
-                                            <Grid item>
-                                                <InputTextFieldValidated fieldType="password" fieldName="password"
-                                                                         label="Passwort" formikProps={props}/>
-                                            </Grid>
-                                                <Grid item className={classes.nextTopic}>
-                                                    <ButtonYellowBig handleClick={props.handleSubmit}
-                                                                     buttonText="Login"/>
-                                                </Grid>
-                                            <Grid item className={classes.nextTopic}>
-                                                <Button
-                                                    className={classes.buttonNonContained}
-                                                    color="primary"
-                                                    onClick={goToRegistration}>
-                                                    Noch keinen Account? Hier geht's zur Registrierung.
-                                                </Button>
-                                            </Grid>
-                                        </Form>
-                                    )
-                                }}
-                            </Formik>
-                        </Grid>
+                        <LoginForm/>
                     </Grid>
                 </Grid>
             </Card>
