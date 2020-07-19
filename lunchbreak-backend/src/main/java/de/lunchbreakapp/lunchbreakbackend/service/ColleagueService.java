206b0e7cc562018e58618d494422ac26e9e14183
Franziska Georg <franzi.georg@web.de>
2020-07-19 16:30:45 +0200

    Remove unused dependencies in backend

--- lunchbreak-backend/src/main/java/de/lunchbreakapp/lunchbreakbackend/service/ColleagueService.java
+++ lunchbreak-backend/src/main/java/de/lunchbreakapp/lunchbreakbackend/service/ColleagueService.java
@@ -7,8 +7,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.aggregation.SampleOperation;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
