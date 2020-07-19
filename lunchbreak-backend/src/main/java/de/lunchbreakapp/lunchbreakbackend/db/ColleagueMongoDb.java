56a718207d7626a04164274ac8470f8f60edd2ca
Franziska Georg <franzi.georg@web.de>
2020-07-19 12:33:27 +0200

    Add method to find colleague by username and get mapping for profile data fetch

--- lunchbreak-backend/src/main/java/de/lunchbreakapp/lunchbreakbackend/db/ColleagueMongoDb.java
+++ lunchbreak-backend/src/main/java/de/lunchbreakapp/lunchbreakbackend/db/ColleagueMongoDb.java
@@ -3,5 +3,10 @@ package de.lunchbreakapp.lunchbreakbackend.db;
 import de.lunchbreakapp.lunchbreakbackend.model.Colleague;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface ColleagueMongoDb extends PagingAndSortingRepository<Colleague, String> {
+
+    public Optional<Colleague> findByUsername(String username);
+
 }
