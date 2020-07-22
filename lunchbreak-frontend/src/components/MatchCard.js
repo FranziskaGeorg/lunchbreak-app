d632bd9b50d3254ece5a8564a629a4bd6038507e
Franziska Georg <franzi.georg@web.de>
2020-07-22 13:35:38 +0200

    Display colleague info on daily match card

--- lunchbreak-frontend/src/components/MatchCard.js
+++ lunchbreak-frontend/src/components/MatchCard.js
@@ -1,20 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import {getRandomColleagueFetch} from "../utils/FetchUtils";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import CardActions from "@material-ui/core/CardActions";
 import ButtonYellowBigPacifico from "./buttons/ButtonYellowBigPacifico";
 import PopupLunchMatch from "./popups/PopupLunchMatch";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Box from "@material-ui/core/Box";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import {FaBriefcase, FaThumbsUp, FaUtensils} from "react-icons/all";
+
+const useStyles = makeStyles((theme) => ({
+    bigBox: {
+        padding: theme.spacing(3)
+    },
+    nextTopicLarge: {
+        paddingTop: theme.spacing(3)
+    },
+    nextTopicSmall: {
+        paddingTop: theme.spacing(1)
+    },
+    icon: {
+        marginRight: theme.spacing(2)
+    }
+}));
 
 export default function MatchCard() {
+    const classes = useStyles();
+
     const [dailyMatch, setDailyMatch] = useState({});
     const [showPopup, setShowPopup] = useState(false);
 
     useEffect(() => {
         getRandomColleagueFetch()
             .then(data => setDailyMatch(data));
-        console.log(dailyMatch);
     }, [])
 
     function handleShuffleClick() {
@@ -26,21 +44,64 @@ export default function MatchCard() {
         setShowPopup(true);
     }
 
+    console.log(dailyMatch);
+
     return (
         <Card>
-            <CardContent>
-                <Typography variant="h3" component="h2">
-                    {dailyMatch.firstName}
-                </Typography>
-                <Typography variant="body2" component="p">
-                    Hi there!
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <ButtonYellowBigPacifico handleClick={handleShuffleClick} buttonText="Mischen"/>
-                <ButtonYellowBigPacifico handleClick={handleLunchClick} buttonText="Lunchen"/>
-            </CardActions>
+            <Box className={classes.bigBox}
+                 display="flex"
+                 flexDirection="column"
+            >
+                <Box>
+                    <Typography variant="h5">
+                        {dailyMatch.firstName}
+                    </Typography>
+                    <Box className={classes.nextTopicLarge}
+                         display="flex"
+                         flexDirection="row"
+                         alignItems="center">
+                        <SvgIcon className={classes.icon} color="primary">
+                            <FaBriefcase/>
+                        </SvgIcon>
+                        <Typography variant="body1">
+                            {dailyMatch.job} ({dailyMatch.subsidiary})
+                        </Typography>
+                    </Box>
+                    <Box className={classes.nextTopicSmall}
+                         display="flex"
+                         flexDirection="row"
+                         alignItems="center">
+                        <SvgIcon className={classes.icon} color="primary">
+                            <FaUtensils/>
+                        </SvgIcon>
+                        <Typography variant="body1">
+                            {dailyMatch.favoriteFood}
+                        </Typography>
+                    </Box>
+                    <Box className={classes.nextTopicSmall}
+                        display="flex"
+                         flexDirection="row"
+                         alignItems="center">
+                        <SvgIcon className={classes.icon} color="primary">
+                            <FaThumbsUp/>
+                        </SvgIcon>
+                    <Typography variant="body1">
+                        {dailyMatch.hobbies}
+                    </Typography>
+                    </Box>
+                </Box>
+                <Box className={classes.nextTopicLarge}
+                     display="flex"
+                     flexDirection="row"
+                     justifyContent="space-around"
+                >
+                    <ButtonYellowBigPacifico handleClick={handleShuffleClick} buttonText="Mischen"/>
+                    <ButtonYellowBigPacifico handleClick={handleShuffleClick} buttonText="Mischen"/>
+                </Box>
+            </Box>
             <PopupLunchMatch showPopup={showPopup} setShowPopup={setShowPopup}/>
         </Card>
     )
 }
+
+/*<ButtonYellowBigPacifico handleClick={handleLunchClick} buttonText="Lunchen"/>*/
