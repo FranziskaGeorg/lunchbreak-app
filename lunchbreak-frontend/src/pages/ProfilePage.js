307889fdf6e91d01d74ed64c05a6c9c336b2f53f
Franziska Georg <franzi.georg@web.de>
2020-07-23 18:04:01 +0200

    Convert profile grids to boxes

--- lunchbreak-frontend/src/pages/ProfilePage.js
+++ lunchbreak-frontend/src/pages/ProfilePage.js
@@ -1,17 +1,16 @@
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
 import ProfileForm from "../components/inputForms/ProfileForm";
+import {Box} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    gridContainer: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(2),
-        marginLeft: theme.spacing(4)
-    },
-    card: {
-        marginBottom: theme.spacing(10)
+    bigBox: {
+        display: "flex",
+        alignItems: "center",
+        backgroundColor: "#ffffff",
+        borderRadius: "10px",
+        padding: theme.spacing(4),
+        marginTop: "40vh"
     }
 }));
 
@@ -19,16 +18,8 @@ export default function ProfilePage() {
     const classes = useStyles();
 
     return (
-        <Card className={classes.card}>
-            <Grid container
-                  className={classes.gridContainer}
-                  direction="column"
-                  alignContent="center"
-                  justify="center"
-                  spacing={1}
-            >
-                <ProfileForm/>
-            </Grid>
-        </Card>
+        <Box className={classes.bigBox}>
+            <ProfileForm/>
+        </Box>
     )
 }
