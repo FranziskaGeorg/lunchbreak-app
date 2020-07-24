13cd1722dda52a7fa0803c7fb09e0cfe52db0743
Franziska Georg <franzi.georg@web.de>
2020-07-24 09:37:10 +0200

    Add overall box to login page

--- lunchbreak-frontend/src/pages/LoginPage.js
+++ lunchbreak-frontend/src/pages/LoginPage.js
@@ -4,12 +4,18 @@ import LoginForm from "../components/inputForms/LoginForm";
 import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
+    overallBox: {
+        display: "flex",
+        justifyContent: "center",
+        width: "100vw",
+        minHeight: "80%"
+    },
     bigBox: {
+        flexGrow: 1,
         display: "flex",
         alignItems: "center",
         backgroundColor: "#ffffff",
         borderRadius: "10px",
-        height: "80%",
         padding: theme.spacing(4)
     }
 }));
@@ -18,8 +24,10 @@ export default function LoginPage() {
     const classes = useStyles();
 
     return (
-        <Box className={classes.bigBox}>
-            <LoginForm/>
+        <Box className={classes.overallBox}>
+            <Box className={classes.bigBox}>
+                <LoginForm/>
+            </Box>
         </Box>
     );
 }
