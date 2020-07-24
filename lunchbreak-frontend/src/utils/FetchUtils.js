efaffb0bd1e09a67d6417724d27877d33c7e9737
Franziska Georg <franzi.georg@web.de>
2020-07-24 15:42:15 +0200

    Add data fetch to save lunch match to db and implement it in daily match card

--- lunchbreak-frontend/src/utils/FetchUtils.js
+++ lunchbreak-frontend/src/utils/FetchUtils.js
@@ -14,6 +14,22 @@ export async function getMatchingColleagueFetch() {
     return await response.json();
 }
 
+export async function saveLunchMatchFetch(matchedUsername) {
+    const token = getJWTToken();
+    const response = await fetch("/api/dailymatch", {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(matchedUsername)
+    });
+    if (response.status !== 200) {
+        throw new Error("Saving profile data failed");
+    }
+    return await response.text();
+}
+
 export async function initProfileDataFetch() {
     const token = getJWTToken();
     const response = await fetch("/api/profile", {
@@ -38,7 +54,6 @@ export async function saveProfileDataFetch(profileData) {
         },
         body: JSON.stringify(profileData)
     });
-    console.log(profileData);
     if (response.status !== 200) {
         throw new Error("Saving profile data failed");
     }
