668e359896e60f91d139d23a4bba8381818826f0
Franziska Georg <franzi.georg@web.de>
2020-07-26 13:22:47 +0200

    Adjust links on lunch match popup

--- lunchbreak-frontend/src/components/popups/PopupLunchMatch.js
+++ lunchbreak-frontend/src/components/popups/PopupLunchMatch.js
@@ -5,8 +5,20 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    link: {
+        color: "#009899",
+        textDecoration: "none",
+        '&:hover': {
+            color: "#dfa528"
+        }
+    }
+}));
 
 export default function PopupLunchMatch({showPopup, setShowPopup, matchData}) {
+    const classes = useStyles();
 
     function handleClosePopup() {
         setShowPopup(false);
@@ -21,15 +33,22 @@ export default function PopupLunchMatch({showPopup, setShowPopup, matchData}) {
         >
             <DialogTitle id="alert-dialog-title">
                 Neues Lunch-Match!
-                <span role="img" aria-labelledby="party-emoji">🎉</span>
+                <span role="img" aria-labelledby="party-emoji"> 🎉</span>
             </DialogTitle>
             <DialogContent>
                 <DialogContentText id="alert-dialog-description">
                     Kontaktiere {matchData.firstName}, um einen Termin zum Lunchen zu vereinbaren.
-                    <br/><br/>E-Mail-Adresse: {matchData.username}
-                    <br/><br/>Handynummer: {matchData.phoneNumber}
                     <br/><br/>
-                    Du kannst die Kontaktdaten deines Matches auch jederzeit zu einem späteren Zeitpunkt in deiner Nachrichtenbox anschauen.
+                    E-Mail-Adresse: <a className={classes.link} href={`mailto:${matchData.username}`}>
+                    {matchData.username}
+                </a>
+                    <br/><br/>
+                    Handynummer: <a className={classes.link} href={`tel:${matchData.phoneNumber}`}>
+                    {matchData.phoneNumber}
+                </a>
+                    <br/><br/>
+                    Du kannst die Kontaktdaten deines Matches auch jederzeit zu einem späteren Zeitpunkt in deiner
+                    Nachrichtenbox anschauen.
                 </DialogContentText>
             </DialogContent>
             <DialogActions>
