e3bb54c08c1883980065250453d94cc2e759289b
Franziska Georg <franzi.georg@web.de>
2020-08-03 14:24:09 +0200

    Adjust all methods in HistoryController

--- lunchbreak-backend/src/main/java/de/lunchbreakapp/lunchbreakbackend/controller/HistoryController.java
+++ lunchbreak-backend/src/main/java/de/lunchbreakapp/lunchbreakbackend/controller/HistoryController.java
@@ -1,9 +1,9 @@
 package de.lunchbreakapp.lunchbreakbackend.controller;
 
-import de.lunchbreakapp.lunchbreakbackend.model.Colleague;
 import de.lunchbreakapp.lunchbreakbackend.model.dto.HistoryData;
 import de.lunchbreakapp.lunchbreakbackend.service.HistoryService;
 import de.lunchbreakapp.lunchbreakbackend.service.MatchService;
+import de.lunchbreakapp.lunchbreakbackend.service.ProfileService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,26 +17,24 @@ import java.util.List;
 public class HistoryController {
 
     private final HistoryService historyService;
-    private final ProfileController profileController;
+    private final ProfileService profileService;
     private final MatchService matchService;
 
-    public HistoryController(HistoryService historyService, ProfileController profileController, MatchService matchService) {
+    public HistoryController(HistoryService historyService, ProfileService profileService, MatchService matchService) {
         this.historyService = historyService;
-        this.profileController = profileController;
+        this.profileService = profileService;
         this.matchService = matchService;
     }
 
     @GetMapping
     public List<HistoryData> getLunchMatchDetails(Principal principal) {
-        Colleague loggedColleague = profileController.getColleagueByUsername(principal);
-        String loggedUsername = loggedColleague.getUsername();
+        String loggedUsername = principal.getName();
         return historyService.getDetailsForLunchMatches(loggedUsername);
     }
 
     @GetMapping("{matchedUsername}")
     public Boolean checkIfMatchIsMutual(Principal principal, @PathVariable String matchedUsername) {
-        Colleague loggedColleague = profileController.getColleagueByUsername(principal);
-        String loggedUsername = loggedColleague.getUsername();
+        String loggedUsername = principal.getName();
         return matchService.isMatchMutual(loggedUsername, matchedUsername);
     }
 
