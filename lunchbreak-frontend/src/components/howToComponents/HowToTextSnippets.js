b7810ea38338647af2c715292057a41345803f57
Franziska Georg <franzi.georg@web.de>
2020-08-04 13:53:10 +0200

    Cleanup howto text snippets and delete unused text field component

--- lunchbreak-frontend/src/components/howToComponents/HowToTextSnippets.js
+++ lunchbreak-frontend/src/components/howToComponents/HowToTextSnippets.js
@@ -6,7 +6,7 @@ import {FaCalendarAlt, FaUser, FaUtensils} from "react-icons/all";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles((theme) => ({
-    stepText: {
+    highlightedText: {
         color: "#009899",
         fontWeight: "bold"
     },
@@ -50,7 +50,7 @@ export function HowToSteps() {
 
     return (
         <Typography>
-            <div className={classes.stepText}>STEP 1: Profil ausfüllen</div>
+            <div className={classes.highlightedText}>STEP 1: Profil ausfüllen</div>
             <p>
                 Fülle nach dem ersten Einloggen Dein <Link className={classes.link} href="/profile">
                 Nutzerprofil
@@ -68,7 +68,7 @@ export function HowToSteps() {
                     <li>Hobbies oder Interessen</li>
                 </ul>
             </p>
-            <div className={classes.stepText}>STEP 2: Matches finden</div>
+            <div className={classes.highlightedText}>STEP 2: Matches finden</div>
             <p>
                 Auf der <Link className={classes.link} href="/dailymatch">
                 Daily Match Page
@@ -81,7 +81,7 @@ export function HowToSteps() {
                 Du hast nun die Wahl, die angezeigte Match Card entweder neu zu mischen oder aber direkt auf "Lunchen"
                 zu klicken.
             </p>
-            <div className={classes.stepText}>STEP 3: Lunchen!</div>
+            <div className={classes.highlightedText}>STEP 3: Lunchen!</div>
             <p>
                 Sobald Dein Lunch Match auf Deiner Match Card ebenfalls "Lunchen" gewählt hat, erscheint ein Popup mit
                 den
@@ -105,25 +105,25 @@ export function Imprint() {
 
     return (
         <Typography>
-            <div className={classes.stepText}>Angaben gemäß § 5 TMG</div><br/>
+            <div className={classes.highlightedText}>Angaben gemäß § 5 TMG</div><br/>
             Franziska Georg<br/>
             Corkstraße 18<br/>
             51103 Köln
             <br/><br/>
-            <div className={classes.stepText}>Vertreten durch:</div>
+            <div className={classes.highlightedText}>Vertreten durch:</div>
             Franziska Georg
             <br/><br/>
-            <div className={classes.stepText}>Kontakt:</div>
+            <div className={classes.highlightedText}>Kontakt:</div>
             franzi.georg@web.de
             <br/><br/>
-            <div className={classes.stepText}>Haftungsausschluss:</div><br/>
-            <div className={classes.stepText}>Haftung für Inhalte</div>
+            <div className={classes.highlightedText}>Haftungsausschluss:</div><br/>
+            <div className={classes.highlightedText}>Haftung für Inhalte</div>
             Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
             <br/><br/>
-            <div className={classes.stepText}>Urheberrecht</div>
+            <div className={classes.highlightedText}>Urheberrecht</div>
             Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
             <br/><br/>
-            <div className={classes.stepText}>Datenschutz</div>
+            <div className={classes.highlightedText}>Datenschutz</div>
             Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.
             Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
             Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
